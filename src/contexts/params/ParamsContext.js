928ca5bed1b1a91e2bd1a479e1e06c8317b6d89c
lucaskbr <lucas_klasa@hotmail.com>
2021-09-25 20:06:14 -0300

    feat: checkpoint

--- src/contexts/params/ParamsContext.js
+++ src/contexts/params/ParamsContext.js
@@ -1,5 +1,4 @@
-import React, { createContext } from 'react';
-import { useState } from 'react/cjs/react.development';
+import React, {useState,  createContext } from 'react';
 
 const ParamsContext = createContext();
 
